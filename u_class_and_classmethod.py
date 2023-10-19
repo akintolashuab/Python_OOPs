4d8933b0c77681f4abe5c822094692f1af00468d
akintola shuaib <akintolashuaib5@gmail.com>
2023-10-19 11:47:19 +0100

    added static method

--- u_class_and_classmethod.py
+++ u_class_and_classmethod.py
@@ -2,16 +2,33 @@ class Date:
     ''' This class return integer of date 
     
     '''
-    def __init__(self, year:int, month:int, day:int) -> int:
+    def __init__(self, year, month, day):
         self.year = year
         self.month = month
         self.day = day
+    
     @classmethod
     def from_string(cls, date_string, sep = '-'):
         ''' This take any input of date format and return normal date 
     '''
         year, month, day = map(int, date_string.split(sep))
         return cls(year, month, day)
+    
+    @staticmethod
+    def add(a,b):
+        return a+b
+    
+    @staticmethod
+    def multiply(a,b):
+        return a*b
+    
+    @staticmethod
+    def power(a,b):
+        return a**b
+    
 
-inst_1 = Date(2023, 12, 35)
+#inst_1 = Date(2023, 12, 35)
+inst_1 = Date.from_string('2023$12$35', '$')
 print(inst_1.day)
+inst_2 = Date.power(2,3)
+print(inst_2)
