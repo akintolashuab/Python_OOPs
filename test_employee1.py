34858576f9e054fb3c7fd2e1e052ca563551111b
akintola shuaib <akintolashuaib5@gmail.com>
2023-11-01 08:40:29 +0100

    set up class and tear down class added

--- test_employee1.py
+++ test_employee1.py
@@ -3,17 +3,26 @@ from employee import Employee
 
 class TestEmployee(unittest.TestCase):
     '''Employee test running'''
-    
+    @classmethod
+    def setUpClass(cls):
+        print("set up class")
+        
+    @classmethod
+    def tearDownClass(cls):
+        print("Tear Down class")
+
     def setUp(self):
+        print('setUp')
         self.emp_1 = Employee('Corey','Schafer',50000)
         self.emp_2 = Employee('Sue','Smith',100000)
-    
+
     def tearDown(self):
-        pass
-        
+        print('tearDown\n')
+
     def test_email(self):
         '''Test for email'''
-        
+        print('test_email')
+
         self.assertEqual(self.emp_1.email, 'Corey.Schafer@gmail.com')
         self.assertEqual(self.emp_2.email, 'Sue.Smith@gmail.com')
         
@@ -25,6 +34,7 @@ class TestEmployee(unittest.TestCase):
         
     def test_fullname(self):
         '''Test for full name'''
+        print('test_fullname')
         
         self.assertEqual(self.emp_1.fullname, 'Schafer Corey')
         self.assertEqual(self.emp_2.fullname, 'Smith Sue')
@@ -33,16 +43,16 @@ class TestEmployee(unittest.TestCase):
         self.emp_1.first = 'Angel'
         self.assertEqual(self.emp_1.fullname, 'Angel Corey')
         self.assertEqual(self.emp_2.fullname, 'Solomon Sue')
-        
+
     def test_apply_raise(self):
         ''' Test for apply raise'''
-       
+        print('test_apply_raise')
+
         self.emp_1.apply_raise()
         self.emp_2.apply_raise()   
         
         self.assertEqual(self.emp_1.pay, 52000)
         self.assertEqual(self.emp_2.pay, 104000)
-        
+
 if __name__ == '__main__':
     unittest.main()
-        
